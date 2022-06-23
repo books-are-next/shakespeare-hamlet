771e059ccc922051f8da2228d6a1e7452db27a4e
jan-martinek <honza.martinek@gmail.com>
2022-06-23 10:30:08 +0000

    Deploying to docs from @ books-are-next/shakespeare-hamlet@3d93baa81284027594ea1a2661206fcf62799d62 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-62a35b1';
+const CACHE = 'cache-3d93baa';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
